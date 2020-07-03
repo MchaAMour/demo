7896afbf450b6d1c27bc8788c3bf66a709a75683
Mcha khamis <eng.mcha@gmail.com>
2020-07-03 15:27:45 +0300

    ADD DOG Function

--- animal.py
+++ animal.py
@@ -2,10 +2,14 @@ import sys
 
 def default():
     print("M7ma meshaa")
-
+def dog():
+    print("Grrrrrrr")
 
 def main():
-    default()
+    if sys.argv[1] == 'dog':
+        dog()
+        else:
+            default()
 
 if  __name__ == '__main__':
     main()
